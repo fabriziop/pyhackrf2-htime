ad51642267b8e11a84da4fa82663246825847f28
Fabrizio Pollastri <mxgbot@gmail.com>
2025-09-13 23:39:02 +0200

    Fix the storage of rf amplifier status (on/off)

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,8 +15,8 @@ project = 'Pyhackrf2 HTime'
 copyright = '2025, Fabrizio Pollastri'
 author = 'Fabrizio Pollastri <mxgbot@gmail.com>'
 
-version = '0.1.1'
-release = '0.1.1'
+version = '0.1.2'
+release = '0.1.2'
 
 
 # -- General configuration ---------------------------------------------------
